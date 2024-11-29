56e34a101c1acf723335b6f80babc2b0bcce9a98
Prachi Parihar <prachisingh30aabb@gmail.com>
2024-11-29 23:23:42 +0530

    Add Code

--- KontactHub/src/main/java/com/Prachi/KontactHub/config/SecurityConfig.java
+++ KontactHub/src/main/java/com/Prachi/KontactHub/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -82,6 +83,10 @@ public class SecurityConfig {
         httpSecurity.logout(logoutForm->{
             logoutForm.logoutUrl("/do-logout");
             logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
+
+        httpSecurity.oauth2Login(oauth->{
+            oauth.loginPage("/login");
         });
         return httpSecurity.build();
     }
